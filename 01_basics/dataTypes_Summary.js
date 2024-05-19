4f7b892c87319ef65adc049bd737c07a7109bae9
Priya-bot12 <119518888+Priya-bot12@users.noreply.github.com>
2024-05-19 15:40:53 +0000

    container config done

--- 01_basics/dataTypes_Summary.js
+++ 01_basics/dataTypes_Summary.js
@@ -23,8 +23,8 @@ const bigNumber = 4545576357653896n
 const heros = ["shaktiman" , "naagraaj"]
 
 let myObj = {
-    name : "Priya"
-    age : 18
+    name : "Priya",
+    age : 18,
 }
 
 const myFunction= function (){     //function Object
@@ -32,3 +32,26 @@ const myFunction= function (){     //function Object
 }
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//**************************************memory************************************
+
+//Stack(Primitive) we get a copy , Heap (Non - Primitive) we get a reference
+
+let myYouTube = "Priya@"
+let anotherName = myYouTube
+anotherName = "chaiaurCode"
+
+console.log(anotherName)
+console.log(myYouTube)
+
+let user1 = {
+    email : "abc@gmail.com",
+    upi : "user@ybl"
+}
+
+let user2 = user1
+
+user2.email = "priya@gmail.com"
+
+console.log(user1.email)
+console.log(user2.email)
