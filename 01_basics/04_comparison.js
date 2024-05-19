65bd455ff6e6ab0b6b4d1f2cb25d4fffbc85963c
Priya-bot12 <119518888+Priya-bot12@users.noreply.github.com>
2024-05-19 11:40:45 +0000

    container config done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,16 @@
+//console.log(2>1)  //>=,<,==,!=
+
+
+console.log("2">1)  //result cannot be predictable 
+
+console.log(null>0)
+console.log(null==0)
+console.log(null<0)
+
+console.log(undefined == 0 )
+console.log(undefined > 0 )
+console.log(undefined < 0 )   //give false  with above three
+
+console.log("2"===2)  //Strict comparison no conversion take place 
+
+
